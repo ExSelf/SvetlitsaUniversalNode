487d498fec41f94ad4d389c3bd6458632340d323
Eugene <savushkin@kinequip.com>
2026-03-06 16:50:44 -0500

    var type change

--- include/constants.c
+++ include/constants.c
@@ -1,5 +1,2 @@
-// Binary pins definitions
-#include <stdbool.h>
-
 /* Binary pins definitions */
-const bool costume_number[8] = {39, 40, 37, 38, 18, 21, 16, 17}; 
+const uint8_t costume_number[8] = {39, 40, 37, 38, 18, 21, 16, 17}; 
