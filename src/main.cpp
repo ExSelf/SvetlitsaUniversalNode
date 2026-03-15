98ff1827b1cb6c768fac5e3325c1c3422ac0a9ab
Eugene Svetlitsa <120709014+ExSelf@users.noreply.github.com>
2026-03-15 16:11:28 -0400

    Refactor pinMode and update serial output
    Updated pinMode to use NodeNumber array and modified output to display NodeNum.

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,22 @@
 #include <esp_now.h>
 #include <ArduinoOTA.h>
 
+uint8_t NodeNum = 0;
 
 void setup() {
 Serial.begin(115200);
 
+
 for (int i = 0; i < 8; i++) {
-    pinMode(costume_number[i], INPUT_PULLUP);
+    pinMode(NodeNumber[i], INPUT_PULLUP);
+
+    NodeNum += (!digitalRead(NodeNumber[i]) * pow(2, i));
   }
+
+Serial.println(NodeNum);
 }
 
 void loop() {
-  Serial.printf(costume_number[0] ? "1" : "0",
-  costume_number[1] ? "1" : "0",
-  costume_number[2] ? "1" : "0",
-  costume_number[3] ? "1" : "0",
-  costume_number[4] ? "1" : "0",
-  costume_number[5] ? "1" : "0",
-  costume_number[6] ? "1" : "0",
-  costume_number[7] ? "1" : "0");
+  Serial.printf("Node Number: %d\n", NodeNum);
   delay(1000);
 }
