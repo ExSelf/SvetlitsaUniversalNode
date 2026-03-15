7daa39cea22a412acaa6dd741ced18b5eb20782f
Eugene Svetlitsa <120709014+ExSelf@users.noreply.github.com>
2026-03-15 16:03:51 -0400

    Add NodeSpecificConstant function
    Implement NodeSpecificConstant function for node constants.

---
+++ include/node_specific_constants.c
@@ -0,0 +1,27 @@
+#include <string.h>
+
+uint16_t NodeSpecificConstant(uint8_t nodeNumber, char *constantName) {
+    if (nodeNumber < 10) {
+
+    } else if (nodeNumber < 20) { //Solaris
+        if (strcmp(constantName, "VOLTAGE_INDEXER") == 0) {
+            return 730;
+        }
+    } else if (nodeNumber < 30) {
+
+    } else if (nodeNumber < 40) {
+
+    } else if (nodeNumber < 50) {
+
+    } else if (nodeNumber < 60) {
+
+    } else if (nodeNumber < 70) {
+
+    } else if (nodeNumber < 80) {
+
+    } else if (nodeNumber < 90) {
+
+    } else if (nodeNumber < 100) {
+
+    }
+}
