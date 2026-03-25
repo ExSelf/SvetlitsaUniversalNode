b06b676ac83bd44ff819855aa2403b7dcfcc6e59
Eugene Svetlitsa <120709014+ExSelf@users.noreply.github.com>
2026-03-25 16:45:45 -0400

    Update WiFi credentials in declarations.cpp

--- src/declarations.cpp
+++ src/declarations.cpp
@@ -3,8 +3,8 @@
 /* Binary pins definitions */
 const uint8_t NodeNumberPins[8] = {39, 40, 37, 38, 18, 21, 16, 17};
 
-const char* ssid = "ExSelf";
-const char* password = "gggggggggg";
+const char* ssid = "test";
+const char* password = "test";
 
 char* hostName;
 
