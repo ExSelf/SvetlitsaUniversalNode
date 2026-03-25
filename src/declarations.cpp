6cc0c825711e2e7c7ace3b675f087723370af0a6
Eugene <savushkin@kinequip.com>
2026-03-25 16:43:01 -0400

    Refactor binary pins definitions: move from definitions.cpp to declarations.cpp and update header files

---
+++ src/declarations.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+/* Binary pins definitions */
+const uint8_t NodeNumberPins[8] = {39, 40, 37, 38, 18, 21, 16, 17};
+
+const char* ssid = "ExSelf";
+const char* password = "gggggggggg";
+
+char* hostName;
+
+uint8_t command;
+uint8_t parameter;
+uint8_t constantCommands[12];
+
+#define VOLTAGE_CHECK_INTERVAL 10
+#define STATUS_SEND_INTERVAL 25
